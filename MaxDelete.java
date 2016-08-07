0d69a53941153064999a942d62877b1e5e58daff
Sivaprakashsiva <sivaprakasam.mcse@ksrce.ac.in>
2016-08-06 23:05:40 -0700

    MaxDelete.java

---
+++ MaxDelete.java
@@ -0,0 +1,25 @@
+public class MaxDelete {
+
+	public static void main(String[] args) {
+		
+		
+		int n=16452;
+		String r="";
+		int len=Integer.toString(n).length();
+		int arr[]=new int[len];
+		int i=0;
+		while(n>0){
+			arr[i]=n%10;
+			//System.out.print(arr[i]);
+			n=n/10;
+			i++;
+		}
+		Arrays.sort(arr);
+		for(int j=0;j<=1;j++){
+			r=""+arr[0]+arr[1];
+			
+		}
+System.out.println(r);
+	}
+
+}
